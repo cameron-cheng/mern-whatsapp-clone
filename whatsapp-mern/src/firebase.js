aa452fd0f6684667391aa2afa48475fcb9bfb97a
cameron cheng <cameroncheng@gmail.com>
2020-11-05 23:10:38 -0800

    initial setup

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmDP3goQHgPVEoyF8LqwPpjf3cMbw6kuE",
+  authDomain: "whatsapp-mern-a6e4f.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-a6e4f.firebaseio.com",
+  projectId: "whatsapp-mern-a6e4f",
+  storageBucket: "whatsapp-mern-a6e4f.appspot.com",
+  messagingSenderId: "562766193908",
+  appId: "1:562766193908:web:4edb42b85de80bcfddeaa1"
+};
